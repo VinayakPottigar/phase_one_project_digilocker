23f0f1a32157bd82a40990a15d4616d2f5fe1e57
VinayakPottigar <vinayak.javadeveloper@gmail.com>
2022-02-23 17:12:52 +0530

    Created company class with its info

---
+++ Virtual Key/src/com/lockedme/digitallocker/Company.java
@@ -0,0 +1,98 @@
+package com.lockedme.digitallocker;
+
+public class Company {
+	private String companyName;
+	private String address;
+	private String establishmentYear;
+	private String owner;
+	private String intro;
+	private long assests;
+	private String websiteLink;
+	
+	private static Company company;
+
+	private Company(String companyName, String address, String establishmentYear, String owner, String intro,
+			long assests, String websiteLink) {
+		this.companyName = companyName;
+		this.address = address;
+		this.establishmentYear = establishmentYear;
+		this.owner = owner;
+		this.intro = intro;
+		this.assests = assests;
+		this.websiteLink = websiteLink;
+	}
+	
+	public static Company getInstance() {
+		if(company==null) {
+			company= new Company("Lockers Pvt. Ltd.","Plot No. 1, Rajiv Gandhi Infotech Park, Hinjewadi, Pune - 411057","1980","Mr. Pottigar Vinayak V.","Provides Cloud based services to keep the digital documents accessible anywhere",(long) 1500.00d,"www.LockedMe.com");
+			
+		}
+		return company;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEstablishmentYear() {
+		return establishmentYear;
+	}
+
+	public void setEstablishmentYear(String establishmentYear) {
+		this.establishmentYear = establishmentYear;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getIntro() {
+		return intro;
+	}
+
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+
+	public long getAssests() {
+		return assests;
+	}
+
+	public void setAssests(long assests) {
+		this.assests = assests;
+	}
+
+	public String getWebsiteLink() {
+		return websiteLink;
+	}
+
+	public void setWebsiteLink(String websiteLink) {
+		this.websiteLink = websiteLink;
+	}
+
+	public static Company getCompany() {
+		return company;
+	}
+
+	public static void setCompany(Company company) {
+		Company.company = company;
+	}
+	
+	
+}
--- Virtual Key/src/com/lockedme/digitallocker/DigiLockerMainApp.java
+++ Virtual Key/src/com/lockedme/digitallocker/DigiLockerMainApp.java
@@ -3,7 +3,19 @@ package com.lockedme.digitallocker;
 public class DigiLockerMainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Company company= Company.getInstance();
+		System.out.println("=================================Company Details====================================================");
+		System.out.println("Name of the Company="+ company.getCompanyName());
+		System.out.println("Address ="+company.getAddress());
+		System.out.println("Establishment Year="+company.getEstablishmentYear());
+		System.out.println("Owner="+company.getOwner());
+		System.out.println("*****Introduction*****"+"\n"+company.getIntro());
+		System.out.println("Assets ="+ company.getAssests()+"cr");
+		System.out.println("Web Site="+ company.getWebsiteLink());
+		System.out.println("====================================================================================================");
+		
+		
+		
 
 	}
 
