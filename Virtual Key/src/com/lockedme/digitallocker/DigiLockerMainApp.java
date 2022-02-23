f439059f92734d92ba2e6c995385443b583cfcee
VinayakPottigar <vinayak.javadeveloper@gmail.com>
2022-02-23 17:50:33 +0530

    Moduler approach followed

--- Virtual Key/src/com/lockedme/digitallocker/DigiLockerMainApp.java
+++ Virtual Key/src/com/lockedme/digitallocker/DigiLockerMainApp.java
@@ -5,11 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class DigiLockerMainApp {
-
-	public static void main(String[] args) {
-		BufferedReader br= null;	
-		String strCurrentLine;
-		Company company= Company.getInstance();
+	
+	public static void printCompanyInfo(Company company) {
 		System.out.println("=================================Company Details====================================================");
 		System.out.println("Name of the Company="+ company.getCompanyName());
 		System.out.println("Address ="+company.getAddress());
@@ -19,30 +16,43 @@ public class DigiLockerMainApp {
 		System.out.println("Assets ="+ company.getAssests()+"cr");
 		System.out.println("Web Site="+ company.getWebsiteLink());
 		System.out.println("====================================================================================================");
+		
+	}
+	
+	public static void printDeveloperInfo() {
+		BufferedReader br= null;	
+		String strCurrentLine;
 		System.out.println("================================This software is Developed by=======================================");
-		
-		
 		try {
 			br = new BufferedReader(new FileReader("C:\\Users\\vinay\\Desktop\\Phase_one_project\\Virtual Key\\src\\com\\lockedme\\digitallocker\\DeveloperInformation"));
 			while ((strCurrentLine = br.readLine()) != null) {
 				System.out.println(strCurrentLine);				    
 			}
-			
+							
 		}
 		catch (IOException e) {
-			   e.printStackTrace();
+			e.printStackTrace();
 		} 
 		finally {
-
-				   try {
-					    if (br != null)
-					    br.close();
-				   } 
-				   catch (IOException ex) {
-					   	ex.printStackTrace();
-			       }		
-
+				
+			try {
+				if (br != null)
+				br.close();
+			} 
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}		
+				
 		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Company company= Company.getInstance();
+		printCompanyInfo(company);
+		printDeveloperInfo();
+		
 		
 
 	}
